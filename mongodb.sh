f507b734565f050b65b9b2814a9ad90288ad6872
Akr <anilkumareddy9757@gmail.com>
2023-05-06 21:21:28 +0530

    echo color

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
-echo -e "\e32m<<<<<<<<< copy mongo repos >>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<< copy mongo repos >>>>>>>>>\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e32m<<<<<<<<<< install mongodb >>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<< install mongodb >>>>>>>>>>\e[0m"
 yum install mongodb-org -y
 
-echo -e "\e32m<<<<<<<<<< start mongodb >>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<< start mongodb >>>>>>>>>\e[0m"
 systemctl enable mongod
 systemctl restart mongod
 
-sed -e -i 's|127.0.0.0|0.0.0.0|' /etc/mongo.conf
+sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
 
 systemctl restart mongod
