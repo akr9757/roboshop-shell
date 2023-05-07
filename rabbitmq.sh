7f6b9a941e2b1fe01ae2b27e1715236cd8a85aa1
Akr <anilkumareddy9757@gmail.com>
2023-05-07 07:22:24 +0530

    echo color

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,15 +1,17 @@
-echo -e "\e32m<<<<<<<<<<< download rabbitmq repos >>>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<< download rabbitmq repos >>>>>>>>>>>\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
-echo -e "\e32m<<<<<<<<<<<<<< install erlang >>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<<<<< install erlang >>>>>>>>>\e[0m"
 yum install erlang -y
 
-echo -e "\e32m<<<<<<<<<<< download rabbitmq repos 2 >>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<< download rabbitmq repos 2 >>>>>>>>>>\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
-echo -e "\e32m<<<<<<<<<<< install rabbitmq >>>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<< install rabbitmq >>>>>>>>>>>\e[0m"
 yum install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+systemctl enable rabbitmq-server
+systemctl restart rabbitmq-server
