fbc7bced78ec5bfaf31d384ab27657a7cee9fe8e
Akr <anilkumareddy9757@gmail.com>
2023-05-07 07:35:19 +0530

    dispatch script

--- dispatch.sh
+++ dispatch.sh
@@ -21,7 +21,7 @@ go mod init dispatch
 go get
 go build
 
-echo -e"\e[32m<<<<<<<<<<<<<<< copy dispatch service >>>>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<<<<<< copy dispatch service >>>>>>>>>>>>\e[0m"
 cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 
