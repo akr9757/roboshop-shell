b49fdedec956eb6e8cc2e853e1dc360be48697ef
Akr <anilkumareddy9757@gmail.com>
2023-05-07 07:31:21 +0530

    dispatch script

--- dispatch.sh
+++ dispatch.sh
@@ -1,22 +1,22 @@
-echo -e "\e32m<<<<<<<<<<< install golang >>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<< install golang >>>>>>>>>>\e[0m"
 yum install golang -y
 
-echo -e "\e32m<<<<<<<<<<<< add application user >>>>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<<< add application user >>>>>>>>>>>>\e[0m"
 useradd roboshop
 
-echo -e "\e32m<<<<<<<<<<< create app directory >>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<< create app directory >>>>>>>>>\e[0m"
 rm -rf /app
 mkdir /app
 
-echo -e "\e32m<<<<<<<<<< download app content >>>>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<< download app content >>>>>>>>>>>>\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
 
-echo -e "\e32m<<<<<<<<<<<<<< unzip app content >>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<<<<< unzip app content >>>>>>>>>\e[0m"
 unzip /tmp/dispatch.zip
 cd /app
 
-echo -e "\e32m<<<<<<<<<<<<<< install dependencies >>>>>>>>>>>>>\e[0m"
+echo -e "\e[32m<<<<<<<<<<<<<< install dependencies >>>>>>>>>>>>>\e[0m"
 go mod init dispatch
 go get
 go build
@@ -25,4 +25,4 @@ echo -e"\e32m<<<<<<<<<<<<<<< copy dispatch service >>>>>>>>>>>>\e[0m"
 cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
