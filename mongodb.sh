82df5c1ede59e705fdc53ee1e4ccdb7c3aa0af89
akr9757 <anilkumareddy9757@gmail.com>
2023-07-20 20:43:41 +0530

    variable

--- mongodb.sh
+++ mongodb.sh
@@ -9,10 +9,9 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[32m<<<<<<<<<< install mongodb >>>>>>>>>>\e[0m"
 yum install mongodb-org -y
 
-echo -e "\e[32m<<<<<<<<<< start mongodb >>>>>>>>>\e[0m"
-systemctl enable mongod
-systemctl restart mongod
-
+echo -e "\e[32m<<<<<<<<<<<< change listen address >>>>>>\e[0m"
 sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/mongod.conf
 
+echo -e "\e[32m<<<<<<<<<< start mongodb >>>>>>>>>\e[0m"
+systemctl enable mongod
 systemctl restart mongod
