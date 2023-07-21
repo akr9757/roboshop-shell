19edee89b20e6ff974f87f883c8d0eff12296be6
akr9757 <anilkumareddy9757@gmail.com>
2023-07-21 17:56:53 +0530

    dispatch service

--- common.sh
+++ common.sh
@@ -1,34 +1,38 @@
 app_user=roboshop
 
+func_print_head() {
+  echo -e '\e[32m<<<<<<<<<<<<< $1 >>>>>>>>>>>\e[0m'
+}
+
 func_nodejs() {
-  echo -e "\e[32m<<<<<<<<<<<<< download nodejs repos >>>>>>>>>>>\e[0m"
+  func_print_head "download nodejs repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  echo -e "\e[32m<<<<<<<<<< install nodejs >>>>>>>>>>\e[0m"
+  func_print_head "install nodejs"
   yum install nodejs -y
 
-  echo -e "\e[32m<<<<<<<<<<<<<<< add application user >>>>>>>>>>>\e[0m"
+  func_print_head "add application user"
   useradd ${app_user}
 
-  echo -e "\e[32m<<<<<<<<<<<<< create app directory >>>>>>>>>>>>\e[0m"
+  func_print_head "create app directory"
   rm -rf /app
   mkdir /app
 
-  echo -e "\e[32m<<<<<<<<<<<< download app content >>>>>>>>>>>>>\e[0m"
+  func_print_head "download app content"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
-  echo -e "\e[32m<<<<<<<<<<<<< unzip app content >>>>>>>>>>\e[0m"
+  func_print_head "unzip app content"
   unzip /tmp/${component}.zip
 
-  echo -e "\e[32m<<<<<<<<<<<<< install dependencies >>>>>>>>>>>>\e[0m"
+  func_print_head "install dependencies"
   cd /app
   npm install
 
-  echo -e "\e[32m<<<<<<<<<<<< copy ${component} service >>>>>>>>>>>\e[0m"
+  func_print_head "copy ${component} service"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-  echo -e "\e[32m<<<<<<<<<<<<<< start user service >>>>>>>>>\e[0m"
+  func_print_head "start user service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
